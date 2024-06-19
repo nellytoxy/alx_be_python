a8b6676d4c5c6f91e2e1073c1dccf7a645ad6b0c
Necodine <nellexighodaro@gmail.com>
2024-06-20 00:47:57 +0100

    this program calculate the simple interest over time

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,8 +1,8 @@
-number1 = 10
-number2 = 5
-addition = number1 + number2
-substraction =number1 - number2
-multiplication = number1 * number2
+number1 = 10  # first variable
+number2 = 5    #seccond variable 
+addition = number1 + number2 # addition of number one and number 2
+substraction =number1 - number2 # substarction of number one and number 2
+multiplication = number1 * number2 # multiplication of number one and number 2
 print ("Addition of number1 and number2 is", (addition))
 print ("Substarction of number1 and number2 is", (substraction))
 print ("Multiplication of number1 and number2 is", (multiplication))
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,5 @@
+p = principal = 1000
+r = rate =0.05
+t = time = 3
+I = interest = p*r*t
+print("The simple interest is:", I)
