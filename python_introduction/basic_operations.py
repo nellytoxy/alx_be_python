1ca8acd7f8d4446ac747a701c144a6ac03191026
Necodine <nellexighodaro@gmail.com>
2024-06-19 23:34:01 +0100

    this program carry out basic mathematical operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+addition = number1 + number2
+substraction =number1 - number2
+multiplication = number1 * number2
+print ("Addition of number1 and number2 is", (addition))
+print ("Substarction of number1 and number2 is", (substraction))
+print ("Multiplication of number1 and number2 is", (multiplication))
