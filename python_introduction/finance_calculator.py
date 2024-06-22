b68e1855ed28121cbb26d9433f53846c3c8e761c
Necodine <nellexighodaro@gmail.com>
2024-06-22 20:00:15 +0100

    simulation to calculate a user savings after one year

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-monthly_income = (int(input("Enter your monthly income: ")))
+monthly_income = int(input("Enter your monthly income: "))
 monthly_Expenses = int(input("Enter your total monthly expenses: "))
 monthly_savings = monthly_income - monthly_Expenses
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
-print(monthly_savings)
-print(projected_savings)
+print("Your monthly savings are", monthly_savings)
+print("Projected savings after one year, with interest, is:", projected_savings)
