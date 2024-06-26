8dde29076aeaa5e054ef15653299e94888b5aaca
Necodine <nellexighodaro@gmail.com>
2024-06-26 23:07:16 +0100

    A python program to control flow using if statement

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,12 +3,12 @@ num2 = int(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /):")
 match operation:
     case operation if operation == "addition":
-        print("result is", num1 + num2)
+        print("The result is", num1 + num2)
     case operation if operation == "substract":
-        print("result is", num1 - num2)
+        print("The result is", num1 - num2)
     case operation if operation == "multiply":
-        print("result is", num1 * num2)
+        print("The result is", num1 * num2)
     case operation if operation == "divide":
-        print("result is", num1 / num2)
+        print("The result is", num1 / num2)
     case _:
         print("Cannot divide by zero.")
