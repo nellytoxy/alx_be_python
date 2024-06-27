1f2f80bb790cbbb3d34450ff57f370c1ef379f0b
Necodine <nellexighodaro@gmail.com>
2024-06-27 23:43:25 +0100

    A python program to control flow using nested loop statement

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,10 +2,10 @@ num=int(input("Enter the size of the pattern: "))
 outer_count = 0
 while outer_count < num:
   # Outer loop controls the number of times the inner loop runs
-  inner_count = 0
+  inner_count = 0 
   for inner_count in range (num):
     # Inner loop repeats for each outer loop iteration
-    print("*"*num, end=" ")
+    print("*" * num, end=" ")
     inner_count +=0
     break
   print()  # Move to a new line after each outer loop iteration
