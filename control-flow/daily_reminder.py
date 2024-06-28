5b2b422212f4f250fc905fe523a3f27713c200b3
Necodine <nellexighodaro@gmail.com>
2024-06-28 03:17:19 +0100

    A python program to control flow using match case statement

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,24 +1,24 @@
-reminder = input("Enter your task: ")
+Reminder = input("Enter your task: ")
 priority=input("Priority (high/medium/low): ")
 time_bound=input("Is it time-bound? (yes/no): ")
 match priority:
-     case priority if priority == "high":
+     case priority if priority == "high":  
         if time_bound=="yes":
-          print(reminder)
-          print(f"{reminder} is a high priority task that requires immediate attention today!")
+          print(Reminder)
+          print(f"{Reminder} is a high priority task that requires immediate attention today!")
         else:
-          print(f"{reminder} is a high priority task but dot requires immediate attention today!")
+          print(f"{Reminder} is a high priority task but do not requires immediate attention today!")
 match priority:
      case priority if priority=="medium":
         if time_bound=="yes":
-          print(reminder)
-          print(f"{reminder} is a medium priority task that requires attention today!")
+          print(f"{Reminder}") 
+          print(f"{Reminder})  is a medium priority task that requires attention today!")
         else:
-          print(f"{reminder} is a medium priority task but do not requires immediate attention today!")
+          print(f"{Reminder}) is a medium priority task but do not requires immediate attention today!")
 match priority:
      case priority if priority == "low":
         if time_bound=="yes":
-          print(reminder)
-          print(f"{reminder} is a low priority task that requires attention before time!")
+          print(f"{Reminder}") 
+          print(f"{Reminder}  a low priority task that requires attention before time")
         else:
-          print(f"{reminder} is a low priority task Consider completing it when you have free time!")
+          print(f"{Reminder}, is a low priority task Consider completing it when you have free time!")
