f4cb754c6a24a30ce76f7faf4ac7e563077b9e2b
Necodine <nellexighodaro@gmail.com>
2024-06-28 05:11:54 +0100

    A python program to control flow using match case statement

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,23 +5,26 @@ match priority:
      case priority if priority == "high":
         match time_bound:
             case time_bound if time_bound== "yes":
-                print(task, "is a high priority task that requires immediate attention today!")
+                print(f"Reminder: {task} is a high priority task that requires immediate attention today!")
             
             case time_bound if time_bound == "no":
-                print(task, "is a high priority task but dot requires immediate attention today!")
+                print(f"Reminder: {task} is a high priority task that does not requires immediate attention now.")
 match priority:
      case priority if priority == "medium":
         match time_bound:
             case time_bound if time_bound== "yes":
-                print(task, "t is a medium priority task that requires your attention today!")
+                print(f"Reminder: {task} is a medium priority task that requires immediate attention today!")
+            
             
             case time_bound if time_bound == "no":
-                print(task, "is a medium priority task but do not requires your attention today!")
+                print(f"Reminder: {task} is a medium priority task that does not requires immediate attention today!")
+            
 match priority:
      case priority if priority == "low":
         match time_bound:
             case time_bound if time_bound == "yes":
-                print(task, " is a low priority that is time bound. Consider completing it before time.!")
+                print(f"Reminder: {task} is a low priority task that requires attention before time!")
             
             case time_bound if time_bound == "no":
-                print(task, "is a low priority task Consider completing it when you have free time.!")
+                print(f"Note: {task} is a low priority task  Consider completing it when you have free time.")
+            
