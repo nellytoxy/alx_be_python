7fb66a8c9253bf80b75a059a1d509e176940ebae
Necodine <nellexighodaro@gmail.com>
2024-06-28 02:46:20 +0100

    A python program to control flow using match case statement

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,21 +1,21 @@
 task = input("Enter your task: ")
 priority=input("Priority (high/medium/low): ")
-time_input=input("Is it time-bound? (yes/no): ")
+time_bound=input("Is it time-bound? (yes/no): ")
 match priority:
      case priority if priority == "high":
-        if time_input == "yes":
-          print(f"{task}, is a high priority task that requires immediate attention today!")
+        if time_bound=="yes":
+          print(f"{task} is a high priority task that requires immediate attention today!")
         else:
-          print(f"{task}, is a high priority task but dot requires immediate attention today!")
+          print(f"{task} is a high priority task but dot requires immediate attention today!")
 match priority:
-     case priority if priority == "medium":
-        if time_input == "yes":
-          print(f"{task}, is a medium priority task that requires attention today!")
+     case priority if priority=="medium":
+        if time_bound=="yes":
+          print(f"{task} is a medium priority task that requires attention today!")
         else:
-          print(f"{task}, is a medium priority task but do not requires immediate attention today!")
+          print(f"{task} is a medium priority task but do not requires immediate attention today!")
 match priority:
      case priority if priority == "low":
-        if time_input == "yes":
-          print(f"{task}, is a low priority task that requires attention before time!")
+        if time_bound=="yes":
+          print(f"{task} is a low priority task that requires attention before time!")
         else:
-          print(f"{task}, is a low priority task Consider completing it when you have free time!")
+          print(f"{task} is a low priority task Consider completing it when you have free time!")
