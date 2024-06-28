8e8e5345e389355dde0924f4200243805cf7e615
Necodine <nellexighodaro@gmail.com>
2024-06-28 02:37:17 +0100

    A python program to control flow using match case statement

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,29 +1,21 @@
 task = input("Enter your task: ")
 priority=input("Priority (high/medium/low): ")
+time_input=input("Is it time-bound? (yes/no): ")
 match priority:
      case priority if priority == "high":
-        time_input=input("Is it time-bound? (yes/no): ")
-        match time_input:
-            case time_input if time_input== "yes":
-                print(task, "is a high priority task that requires immediate attention today!")
-            
-            case time_input if time_input == "no":
-                print(task, "is a high priority task but dot requires immediate attention today!")
+        if time_input == "yes":
+          print(f"{task}, is a high priority task that requires immediate attention today!")
+        else:
+          print(f"{task}, is a high priority task but dot requires immediate attention today!")
 match priority:
      case priority if priority == "medium":
-        time_input=input("Is it time-bound? (yes/no)")
-        match time_input:
-            case time_input if time_input== "yes":
-                print(task, "t is a medium priority task that requires your attention today!")
-            
-            case time_input if time_input == "no":
-                print(task, "is a medium priority task but do not requires your attention today!")
+        if time_input == "yes":
+          print(f"{task}, is a medium priority task that requires attention today!")
+        else:
+          print(f"{task}, is a medium priority task but do not requires immediate attention today!")
 match priority:
      case priority if priority == "low":
-        time_input=input("Is it time-bound? (yes/no)")
-        match time_input:
-            case time_input if time_input == "yes":
-                print(task, " is a low priority that is time bound. Consider completing it before time.!")
-            
-            case time_input if time_input == "no":
-                print(task, "is a low priority task Consider completing it when you have free time.!")
+        if time_input == "yes":
+          print(f"{task}, is a low priority task that requires attention before time!")
+        else:
+          print(f"{task}, is a low priority task Consider completing it when you have free time!")
