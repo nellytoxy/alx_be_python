8cf05b886c0a490a2be932ca79f3d3c69ea8bc5d
Necodine <nellexighodaro@gmail.com>
2024-07-07 15:30:14 +0100

    A simple python program that convert temperature

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,9 +1,12 @@
-def f_to_c (FAHRENHEIT_TO_CELSIUS_FACTOR):
-    f_to_c_value = (FAHRENHEIT_TO_CELSIUS_FACTOR - 32)* (5/9)
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+def convert_to_celsius(fahrenheit):
+    f_to_c_value = (fahrenheit - 32)* FAHRENHEIT_TO_CELSIUS_FACTOR
     return f_to_c_value
 
-def c_to_f (CELSIUS_TO_FAHRENHEIT_FACTOR):
-    c_to_f_value =  (CELSIUS_TO_FAHRENHEIT_FACTOR * (9/5)) + 32
+
+def convert_to_fahrenheit(celsius):
+    c_to_f_value =  ( celsius* CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     return c_to_f_value
 
 
@@ -13,18 +16,18 @@ while True:
     print ("2 for Fareinheit")
     print ("3 for exit")
 
-    user_temp = input("Enter (1,2,3) for the temperature the user will like to convert to")
+    user_temp = int(input("Enter (1,2,3) for the temperature the user will like to convert to: "))
 
     if user_temp == 1:
-        temp_value = input("enter the temp")
+        temp_value = input("Enter the temp: ")
         temp_value = float(temp_value)
-        print(temp_value," degree Fareinheit is", f'{f_to_c(temp_value): 2f}', "degres fareinheit")
+        print(temp_value," degree Fareinheit is", f'{convert_to_celsius(temp_value)}', "degres fareinheit")
 
     elif user_temp == 2:
         temp_value = input("enter the temp")
         temp_value = float(temp_value)
         
-        print(temp_value," celsius is", f'{c_to_f(temp_value): 2f}', "degres celsius")
+        print(temp_value," celsius is", f'{convert_to_fahrenheit(temp_value)}', "degres celsius")
 
     elif user_temp == 3:
         print ("program as been closed")
