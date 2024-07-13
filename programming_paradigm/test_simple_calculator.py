67290a985e019046105ed4f866b15ae4ef712618
Necodine <nellexighodaro@gmail.com>
2024-07-13 17:43:39 +0100

    A simple python program that carry unitest on python

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,7 +11,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         # Add more assertions to thoroughly test the add method.
-    def test_substraction(self):
+    def test_subtraction(self):
         
         self.assertEqual(self.calc.subtract(2, 3), -1)
         self.assertEqual(self.calc.subtract(-1, 1), -2)
