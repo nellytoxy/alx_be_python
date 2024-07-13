27f913dbe0716e0da28ba51da52727391f178c4b
Necodine <nellexighodaro@gmail.com>
2024-07-13 12:05:23 +0100

    A simple python program that carry out banking function

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,20 +1,20 @@
 class BankAccount:
-    def __init__(self,name):
-        self.name=name
-        self.balance = 0
+    def __init__(self,account_balance=0):
+        self.account_balance=account_balance
 
     def deposit(self,amount):
         self.amount=amount
-        self.balance=self.balance + self.amount
-        print(f"{self.balance}, $")
+        self.account_balance = self.account_balance + self.amount
+       # print(f"$ {self.account_balance}")
     def withdraw(self,amount):
         self.amount=amount
-        if self.balance < self.amount:
-            print(f"insufficient funds please top up")
+        if self.account_balance > self.amount:
+             self.account_balance = self.account_balance - self.amount
+             return True
         else:
-            self.balance=self.balance - self.amount
-            print(f"{self.balance}, $")
+             return False
+        
     def display_balance(self):
-        display_balance=self.balance
-        print(f"{display_balance}, Current Balance:")
+        self.display_balance=self.display_balance
+        print(f"Current Balance: {self.display_balance:.2f},")
         
