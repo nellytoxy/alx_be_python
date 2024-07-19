4c8c0a4aca798b22e69efb7694cda2971f0a21bf
Necodine <nellexighodaro@gmail.com>
2024-07-19 02:14:36 +0100

    A simple python to practice oop

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,44 +1,34 @@
-# library_system.py
-
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size} KB"
+
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
 
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
 class Library:
     def __init__(self):
         self.books = []
-
-    def __str__(self):
-        pass
 
     def add_book(self, book):
         self.books.append(book)
 
     def list_books(self):
-        #print("Listing all books in the library:")
         for book in self.books:
-            if isinstance(book, Book):
-                book_type = "Book"
-                if isinstance(book, EBook):
-                    book_type = "EBook"
-                elif isinstance(book, PrintBook):
-                    book_type = "PrintBook"
-                print(f"{book_type}: {book.title} by {book.author}")
-                if isinstance(book, EBook):
-                    book_type = "EBook"
-                    print(f"{book_type}: {book.title} by {book.author}, File Size: {book.file_size}KB")
-                elif isinstance(book, PrintBook):
-                    book_type = "PrintBook"
-                    print(f"{book_type}: {book.title} by {book.author}, Page Count: {book.page_count}")
-                    
+            print(book)
