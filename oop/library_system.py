6b66c972f10480175584949ee3112314838564b2
Necodine <nellexighodaro@gmail.com>
2024-07-19 01:52:21 +0100

    A simple python to practice oop

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,5 +1,7 @@
+# library_system.py
+
 class Book:
-    def __init__(self,title,author):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
 
@@ -9,21 +11,19 @@ class EBook(Book):
         self.file_size = file_size
 
 class PrintBook(Book):
-    def __init__(self,title,author,page_count):
-        super().__init__(title,author)
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
         self.page_count = page_count
 
 class Library:
     def __init__(self):
         self.books = []
-    def __str__(self):
-        pass
 
     def add_book(self, book):
         self.books.append(book)
 
     def list_books(self):
-        print("print details of each book")
+        #print("Listing all books in the library:")
         for book in self.books:
             if isinstance(book, Book):
                 book_type = "Book"
@@ -33,6 +33,9 @@ class Library:
                     book_type = "PrintBook"
                 print(f"{book_type}: {book.title} by {book.author}")
                 if isinstance(book, EBook):
-                    print(f"   File size: {book.file_size} MB")
+                    book_type = "EBook"
+                    print(f"{book_type}: {book.title} by {book.author}, File Size: {book.file_size}KB")
                 elif isinstance(book, PrintBook):
-                    print(f"   Page count: {book.page_count}")
+                    book_type = "PrintBook"
+                    print(f"{book_type}: {book.title} by {book.author}, Page Count: {book.page_count}")
+                    
